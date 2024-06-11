9057b0f0117b25ad85f120c7b7cd8761e27ce6e5
rafaelMachado1891 <rafadmn1@gmail.com>
2024-06-10 23:50:47 -0300

    projeto final

--- coleta/coleta/settings.py
+++ coleta/coleta/settings.py
@@ -91,5 +91,5 @@ ROBOTSTXT_OBEY = False
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-#TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
